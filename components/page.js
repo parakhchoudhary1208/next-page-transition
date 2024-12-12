08a1ae6b35b2679c21bac3970ba0a67697f8e7c2
parakh2101 <parakhchoudhary1999@gmail.com>
2024-12-12 11:43:32 +0530

    first commit

---
+++ components/page.js
@@ -0,0 +1,12 @@
+import Header from "./header";
+
+export default function Page({children}) {
+    return(
+        <>
+            <Header/>
+            <main>
+                {children}
+            </main>
+        </>
+    )
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '',
+    output: 'export',
+    distDir: 'out',
+};
 
 export default nextConfig;
