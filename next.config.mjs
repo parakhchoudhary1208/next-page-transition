2747db09ea0aff093291b09dbcbcafca5a4928d0
parakh2101 <parakhchoudhary1999@gmail.com>
2024-12-12 12:30:15 +0530

    repo added

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/next-page-transition/' : '',
     output: 'export',
     distDir: 'out',
 };
