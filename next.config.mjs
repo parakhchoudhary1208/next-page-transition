2d7e420f17ba2f0f606ec1b36a54cebb52903109
parakh2101 <parakhchoudhary1999@gmail.com>
2025-02-15 11:09:53 +0530

    Next Rebuild Deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/next-page-transition/' : '',
+    // assetPrefix: process.env.NODE_ENV === 'production' ? '/next-page-transition/' : '',
     output: 'export',
     distDir: 'out',
 };
